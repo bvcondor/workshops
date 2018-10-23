6e5056e04ccd218c291e58e51f0d6963767bd096
= <byronvinicio13@gmail.com>
2018-10-22 19:15:23 -0500

    workshop#2

---
+++ first partial/workshop#2/StartingWithObjects/addition.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+using namespace std;
+main(){
+  int addent1, addent2, result;
+  addent1=10;
+  addentes2=23;
+  result=0;
+  result=addent1+addent2;
+  cout<<"The RESULT IS: "<<tot<<endl;
+  system("system");
+  printf ("the first number is:%d,addent1");
+}
